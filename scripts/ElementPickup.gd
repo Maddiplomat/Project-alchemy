cb8656a6a407d1bcf587e0651a8e6ac33db7bc8a
Maddiplomat <divyansh.mishra@ultrahuman.com>
2026-05-11 05:43:15 +0530

    Add get_element_id helper to ElementPickup for scanner support

--- scripts/ElementPickup.gd
+++ scripts/ElementPickup.gd
@@ -55,7 +55,9 @@ func _attempt_pickup() -> void:
 	if item_data.is_empty():
 		return
 
-	InventoryManager.add_element(item_data.id, pickup_quantity, 1.0)
+	if not InventoryManager.add_element(item_data.id, pickup_quantity, 1.0):
+		return
+
 	picked_up.emit(item_data, pickup_quantity)
 	prompt_label.visible = false
 	queue_free()
@@ -132,3 +134,9 @@ func _play_idle_animation() -> void:
 	var anim_name := "idle_" + str(resolved_element_id)
 	if anim_player.has_animation(anim_name):
 		anim_player.play(anim_name)
+
+
+func get_element_id() -> StringName:
+	if not element_id.is_empty():
+		return element_id
+	return get_meta(&"element_id", &"")
